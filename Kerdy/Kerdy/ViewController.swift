09f2e9a53c52fd1f6d4c44a65c21b2d9a00ee39b
taipaise <taipaise@gmail.com>
2023-10-20 12:09:16 +0900

    [Add] 1 프로젝트 생성

---
+++ Kerdy/Kerdy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kerdy
+//
+//  Created by 이동현 on 2023/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
