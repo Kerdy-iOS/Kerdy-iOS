a9ff5ba4d6054ae97690369d0384e38477329b93
jeongdung-eo <jeuniis2@gmail.com>
2023-10-31 17:35:17 +0900

    [Feat] 16 탭바 로직 구현

---
+++ Kerdy/Kerdy/TabBar/TabBarVC.swift
@@ -0,0 +1,62 @@
+//
+//  TabBarVC.swift
+//  Kerdy
+//
+//  Created by JEONGEUN KIM on 10/31/23.
+//
+
+import UIKit
+
+import Core
+
+final class TabBarVC: UITabBarController {
+
+    // MARK: - Property
+    
+    private var tabs: [UIViewController] = []
+    private lazy var defaultTabBarHeight = { tabBar.frame.size.height }()
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setTabBarItems()
+        setTabBarUI()
+    }
+
+}
+
+// MARK: - Methods
+
+extension TabBarVC {
+    
+    func setTabBarItems() {
+        tabs = [MyCardVC(),
+                EventVC(),
+                ChatVC(),
+                SettingVC()
+        ]
+        
+        TabBarItemType.allCases.forEach {
+            tabs[$0.rawValue].tabBarItem = $0.setTabBarItem()
+            tabs[$0.rawValue].tabBarItem.tag = $0.rawValue
+            tabs[$0.rawValue].tabBarItem.imageInsets = UIEdgeInsets.init(
+                top: 10,
+                left: 0,
+                bottom: -10,
+                right: 0
+            )
+        }
+        
+        setViewControllers(tabs, animated: false)
+    }
+    
+    func setTabBarUI() {
+        tabBar.configureTabBar()
+        tabBar.backgroundColor = .kerdyBackground
+        tabBar.tintColor = .kerdyMain
+        tabBar.itemPositioning = .centered
+        tabBar.layer.applyShadow()
+    }
+}
