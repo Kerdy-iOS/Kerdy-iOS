b5db991048e3d5105095a23d0d2ed2fb19100b09
jeongdung-eo <jeuniis2@gmail.com>
2023-11-01 22:11:49 +0900

    [Add] 18 알림 설정 UI 리터럴 추가

--- Kerdy/Kerdy/Setting/Strings.swift
+++ Kerdy/Kerdy/Setting/Strings.swift
@@ -13,4 +13,10 @@ struct Strings {
     /// setting
     static let article = "작성한 글"
     static let comments = "작성한 댓글"
+    
+    ///notification
+    static let notificationTitle = "알림 설정"
+    static let notification = "알림 수신"
+    static let tag = "태그 알림"
+    static let subTag = "태그 알림 설정 시 태그 관련 게시물 업로드 알림을 보내드립니다."
 }
