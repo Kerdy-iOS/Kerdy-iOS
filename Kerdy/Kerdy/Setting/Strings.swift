c4528a8c0d838e07ed46392c6ef98178510f2a4c
jeongdung-eo <jeuniis2@gmail.com>
2023-11-02 11:15:30 +0900

    [Add] 20 태그 알림 리터럴 추가

--- Kerdy/Kerdy/Setting/Strings.swift
+++ Kerdy/Kerdy/Setting/Strings.swift
@@ -17,6 +17,10 @@ struct Strings {
     ///notification
     static let notificationTitle = "알림 설정"
     static let notification = "알림 수신"
-    static let tag = "태그 알림"
+    static let notificationTag = "태그 알림"
     static let subTag = "태그 알림 설정 시 태그 관련 게시물 업로드 알림을 보내드립니다."
+    
+    ///TagNotification
+    static let register = "등록하기"
+    static let tag = "태그"
 }
