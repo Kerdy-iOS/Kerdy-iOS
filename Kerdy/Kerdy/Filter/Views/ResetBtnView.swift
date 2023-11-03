92178b27a78dda9daeaaa258ad4e751bc680cfa0
taipaise <taipaise@gmail.com>
2023-11-03 22:25:55 +0900

    [DESIGN] 28 전체 초기화 버튼 UI 구현

--- Kerdy/Kerdy/Filter/Views/ResetBtnView.swift
+++ Kerdy/Kerdy/Filter/Views/ResetBtnView.swift
@@ -6,7 +6,58 @@
 //
 
 import UIKit
+import SnapKit
 
 class ResetBtnView: UIView {
 
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "ic_reset")
+        return imageView
+    }()
+
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "전체 초기화"
+        label.font = .nanumSquare(to: .bold, size: 12)
+        label.textColor = .kerdyGray02
+        return label
+    }()
+
+    private lazy var resetBtn: UIButton = {
+        let button = UIButton()
+        button.setTitle(nil, for: .normal)
+        button.backgroundColor = .clear
+        return button
+    }()
+
+    init() {
+        super.init(frame: .zero)
+        setLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setLayout() {
+        addSubviews(imageView)
+        addSubviews(titleLabel)
+        addSubviews(resetBtn)
+
+        imageView.snp.makeConstraints{
+            $0.size.equalTo(12)
+            $0.leading.equalToSuperview()
+            $0.centerY.equalToSuperview()
+        }
+
+        titleLabel.snp.makeConstraints {
+            $0.verticalEdges.leading.equalTo(imageView.snp.trailing).offset(1)
+            $0.height.equalTo(14)
+        }
+
+        resetBtn.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
 }
