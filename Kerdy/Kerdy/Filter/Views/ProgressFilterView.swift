ea9a3c0acbeb8385cb56b01307bda7da171eed16
taipaise <taipaise@gmail.com>
2023-11-05 23:19:37 +0900

    [DESIGN] 28 진행상태 필터 view 구현

--- Kerdy/Kerdy/Filter/Views/ProgressFilterView.swift
+++ Kerdy/Kerdy/Filter/Views/ProgressFilterView.swift
@@ -6,7 +6,90 @@
 //
 
 import UIKit
+import Core
 
 class ProgressFilterView: UIView {
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "진행 상태"
+        label.font = .nanumSquare(to: .bold, size: 15)
+        return label
+    }()
 
+    lazy var progressStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 8
+        stackView.distribution = .equalSpacing
+        return stackView
+    }()
+
+    private lazy var divideLine = DivideLine(frame: .zero, backgroundColor: .kerdyGray01)
+
+    lazy var inProgressBtn = FilterBtn(title: "진행중", tag: 0)
+    lazy var isPlanned = FilterBtn(title: "진행예정", tag: 0)
+    lazy var isFinished = FilterBtn(title: "마감", tag: 0)
+    
+    init() {
+        super.init(frame: .zero)
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setLayout() {
+        addSubviews(titleLabel)
+        addSubviews(progressStackView)
+        addSubviews(divideLine)
+        setProgressStackViewLayout()
+
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(25)
+            $0.leading.equalToSuperview().offset(17)
+            $0.size.width.equalTo(28).priority(250)
+            $0.size.height.equalTo(19).priority(250)
+        }
+
+        progressStackView.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(14)
+            $0.leading.equalToSuperview().offset(17)
+            $0.size.width.equalTo(250).priority(250)
+            $0.size.height.equalTo(33).priority(250)
+        }
+
+        divideLine.snp.makeConstraints {
+            $0.top.equalTo(progressStackView.snp.bottom).offset(26)
+            $0.horizontalEdges.equalToSuperview()
+            $0.size.height.equalTo(0.5)
+        }
+    }
+
+    private func setProgressStackViewLayout() {
+        progressStackView.addArrangedSubview(inProgressBtn)
+        progressStackView.addArrangedSubview(isPlanned)
+        progressStackView.addArrangedSubview(isFinished)
+
+        inProgressBtn.snp.makeConstraints {
+            $0.height.equalTo(32)
+            $0.width.equalTo(86)
+        }
+
+        isPlanned.snp.makeConstraints {
+            $0.height.equalTo(32)
+            $0.width.equalTo(98)
+        }
+
+        isFinished.snp.makeConstraints {
+            $0.height.equalTo(32)
+            $0.width.equalTo(74)
+        }
+    }
+
+    func setUI() {
+        inProgressBtn.setLayer()
+        isPlanned.setLayer()
+        isFinished.setLayer()
+    }
 }
