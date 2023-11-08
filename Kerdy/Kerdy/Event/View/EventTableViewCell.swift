7fddb5bda2032997902def90c509bcbcec96d31d
taipaise <taipaise@gmail.com>
2023-11-09 00:57:19 +0900

    Squashed commit of the following:
    commit cfedbce62948564ce9846849f84c04cadd80236d
    Author: taipaise <taipaise@gmail.com>
    Date:   Mon Nov 6 13:13:45 2023 +0900
    
        [DESIGN] 32 행사 tableView cell UI 최신화

--- Kerdy/Kerdy/Event/View/EventTableViewCell.swift
+++ Kerdy/Kerdy/Event/View/EventTableViewCell.swift
@@ -7,21 +7,40 @@
 
 import UIKit
 import SnapKit
+import Core
 
 final class EventTableViewCell: UITableViewCell {
 
     lazy var titleLabel: UILabel = {
         let label = UILabel()
-        label.font = .nanumSquare(to: .bold, size: 14)
-        label.numberOfLines = 3
+        label.font = .nanumSquare(to: .bold, size: 19)
+        label.numberOfLines = 2
         return label
     }()
 
-    lazy var progressLabel: UILabel = {
+    lazy var dDayLabel: UILabel = {
         let label = UILabel()
-        label.font = .nanumSquare(to: .bold, size: 12)
+        label.text = "D-14"
+        label.font = .nanumSquare(to: .extraBold, size: 13)
+        label.textColor = .kerdyMain
         return label
     }()
+
+    lazy var priceLabel: UILabel = {
+        let label = UILabel()
+        label.text = "무료"
+        label.font = .nanumSquare(to: .regular, size: 12)
+        return label
+    }()
+
+    lazy var placeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "온라인"
+        label.font = .nanumSquare(to: .regular, size: 12)
+        return label
+    }()
+
+    lazy var divideLine = DivideLine(frame: .zero, backgroundColor: .kerdyGray01)
 
     lazy var tagStackView: UIStackView = {
         let stackView = UIStackView()
@@ -49,36 +68,63 @@ final class EventTableViewCell: UITableViewCell {
     }
 
     private func setLayout() {
-        contentView.addSubview(tagStackView)
-        contentView.addSubview(titleLabel)
-        contentView.addSubview(progressLabel)
         contentView.addSubview(eventImage)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(dDayLabel)
+        contentView.addSubview(priceLabel)
+        contentView.addSubview(divideLine)
+        contentView.addSubview(placeLabel)
+        contentView.addSubview(tagStackView)
 
-        tagStackView.snp.makeConstraints {
+        eventImage.snp.makeConstraints {
+            $0.height.equalTo(180)
+            $0.width.equalTo(326).priority(250)
+            $0.top.equalToSuperview().offset(10)
             $0.leading.equalToSuperview().offset(17)
-            $0.top.equalToSuperview().offset(26)
-            $0.height.equalTo(22)
-            $0.width.equalTo(194).priority(250)
+            $0.trailing.equalToSuperview().offset(-17)
+        }
+
+        dDayLabel.snp.makeConstraints {
+            $0.height.equalTo(15)
+            $0.width.equalTo(31)
+            $0.top.equalTo(eventImage.snp.bottom).offset(12)
+            $0.leading.equalToSuperview().offset(17)
         }
 
         titleLabel.snp.makeConstraints {
+            $0.height.equalTo(19).priority(250)
+            $0.width.equalTo(326).priority(250)
             $0.leading.equalToSuperview().offset(17)
-            $0.top.equalTo(tagStackView.snp.bottom).offset(12)
-            $0.height.equalTo(36).priority(250)
-            $0.width.equalTo(177)
-        }
-
-        progressLabel.snp.makeConstraints {
-            $0.leading.equalToSuperview().offset(17)
-            $0.bottom.equalToSuperview().offset(16)
-            $0.height.equalTo(33)
-        }
-
-        eventImage.snp.makeConstraints {
-            $0.height.equalTo(126)
-            $0.width.equalTo(100)
-            $0.centerY.equalToSuperview()
             $0.trailing.equalToSuperview().offset(-17)
+            $0.top.equalTo(dDayLabel.snp.bottom).offset(9)
+        }
+
+        priceLabel.snp.makeConstraints {
+            $0.height.equalTo(14)
+            $0.width.equalTo(22).priority(250)
+            $0.leading.equalToSuperview().offset(17)
+            $0.top.equalTo(titleLabel.snp.bottom).offset(6)
+        }
+
+        divideLine.snp.makeConstraints {
+            $0.height.equalTo(7)
+            $0.width.equalTo(1)
+            $0.leading.equalTo(priceLabel.snp.trailing).offset(6)
+            $0.top.equalTo(priceLabel.snp.top)
+        }
+
+        placeLabel.snp.makeConstraints {
+            $0.height.equalTo(14)
+            $0.width.equalTo(33).priority(250)
+            $0.leading.equalTo(divideLine.snp.trailing).offset(6)
+            $0.top.equalTo(priceLabel.snp.top)
+        }
+
+        tagStackView.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(17)
+            $0.top.equalTo(priceLabel.snp.bottom).offset(21)
+            $0.height.equalTo(22)
+            $0.width.equalTo(20).priority(250)
         }
     }
 
@@ -87,10 +133,6 @@ final class EventTableViewCell: UITableViewCell {
         for index in 0...2 {
             tagStackView.addArrangedSubview(TagView())
             tagStackView.arrangedSubviews[index].isHidden = true
-        }
-
-        tagStackView.snp.makeConstraints { make in
-            make.width.equalTo(10).priority(250)
         }
     }
 }
