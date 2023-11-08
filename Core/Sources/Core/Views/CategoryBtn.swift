c97f6fbaab6084419154ac8a5d5a741425de3b87
ekrud99 <dagyeong33@gmail.com>
2023-11-09 03:02:23 +0900

    [Fix] 34 컨벤션 수정

---
+++ Core/Sources/Core/Views/CategoryBtn.swift
@@ -0,0 +1,25 @@
+//
+//  CategoryBtn.swift
+//  Kerdy
+//
+//  Created by 최다경 on 11/9/23.
+//
+
+import UIKit
+
+public class CategoryBtn: UIButton {
+    
+    public init(target: Any?, action: Selector, title: String) {
+        super.init(frame: .zero)
+        self.setTitle(title, for: .normal)
+        self.setTitleColor(.black, for: .normal)
+        self.addTarget(target, action: action, for: .touchUpInside)
+    }
+    
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+
