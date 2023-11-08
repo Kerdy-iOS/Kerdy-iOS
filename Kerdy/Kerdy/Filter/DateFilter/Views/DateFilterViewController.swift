feaa94b18a1c4dc6527a831246f22561943083da
taipaise <taipaise@gmail.com>
2023-11-08 18:20:30 +0900

    [Move] 28 날짜 선택 FilterVC 추가를 위해 Filter 폴더 구조 변경

---
+++ Kerdy/Kerdy/Filter/DateFilter/Views/DateFilterViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DateFilterViewController.swift
+//  Kerdy
+//
+//  Created by 이동현 on 11/8/23.
+//
+
+import UIKit
+
+class DateFilterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
