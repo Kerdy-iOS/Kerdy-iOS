a77112546b98711e5e5cb686d1a4bf778ce4ad03
jeongdung-eo <jeuniis2@gmail.com>
2023-12-01 18:15:48 +0900

    [Feat] 26 SettingViewModel 로직 구현

--- Kerdy/Kerdy/Setting/Model/SettingViewModel.swift
+++ Kerdy/Kerdy/Setting/Model/SettingViewModel.swift
@@ -11,65 +11,99 @@ import RxSwift
 import RxRelay
 import RxCocoa
 
+import Moya
+
 final class SettingViewModel: ViewModelType {
     
+    // MARK: - Property
+    
     var disposeBag = DisposeBag()
+    private let settingManager: SettingManager
+    private let id = Int(KeyChainManager.read(forkey: .memberId) ?? "0") 
+    private let profileList = BehaviorRelay<MemberProfileResponseDTO>(value: .empty())
     
+    // MARK: - Init
+    
+    init(settingManager: SettingManager) {
+        self.settingManager = settingManager
+    }
+
     struct Input {
-        let viewWillAppear: ControlEvent<Bool>
+        let viewWillAppear: Driver<Bool>
     }
     
-    struct CellInput {
-        let tapProfileButton: Signal<Void>
-        let tapArticleButton: Signal<Void>
-        let tapCommentsButton: Signal<Void>
-    }
+//    struct CellInput {
+//        let tapArticleButton: Signal<Void>
+//        let tapCommentsButton: Signal<Void>
+//    }
     
     struct Output {
-        let settingList: BehaviorRelay<[ProfileResponseDTO]>
+        let settingList: BehaviorRelay<MemberProfileResponseDTO>
     }
-    
-    struct CellOutput {
-        let didProfileButtonTapped: Signal<Void>
-        let didArticleButtonTapped: Signal<Void>
-        let didCommentsButtonTapped: Signal<Void>
-    }
+//    
+//    struct CellOutput {
+//        let didArticleButtonTapped: Signal<Void>
+//        let didCommentsButtonTapped: Signal<Void>
+//    }
     
     func transform(input: Input) -> Output {
-        let profileList = BehaviorRelay(value: [ProfileResponseDTO.dummy()])
         
         let output = Output(settingList: profileList)
         
         input.viewWillAppear
-            .withLatestFrom(profileList)
-            .subscribe(with: self, onNext: { _, updatedProfileList in
-                output.settingList.accept(updatedProfileList)
+            .asDriver()
+            .drive(with: self, onNext: { owner, _ in
+                guard let id = self.id else { return }
+                owner.getMember(id: id)
             })
             .disposed(by: disposeBag)
+
         return output
     }
+//    
+//    func transform(input: CellInput) -> CellOutput {
+//        let didArticleButtonTapped = PublishRelay<Void>()
+//        let didCommentsButtonTapped = PublishRelay<Void>()
+//        
+//        let output = CellOutput(didArticleButtonTapped: didArticleButtonTapped.asSignal(),
+//                                didCommentsButtonTapped: didCommentsButtonTapped.asSignal())
+//        
+//        input.tapArticleButton
+//            .asObservable()
+//        .take(1)
+//        .bind(to: didCommentsButtonTapped)
+//            .disposed(by: disposeBag)
+//        
+//        input.tapCommentsButton
+//            .asObservable()
+//            .take(1)
+//            .bind(to: didCommentsButtonTapped)
+//            .disposed(by: disposeBag)
+//        
+//        return output
+//    }
+}
+
+extension SettingViewModel {
     
-    func transform(input: CellInput) -> CellOutput {
-        let didProfileButtonTapped = PublishRelay<Void>()
-        let didArticleButtonTapped = PublishRelay<Void>()
-        let didCommentsButtonTapped = PublishRelay<Void>()
-        
-        let output = CellOutput(didProfileButtonTapped: didProfileButtonTapped.asSignal(),
-                                didArticleButtonTapped: didArticleButtonTapped.asSignal(),
-                                didCommentsButtonTapped: didCommentsButtonTapped.asSignal())
-        
-        input.tapProfileButton
-            .emit()
+    func getMember(id: Int) {
+        settingManager.getMemger(id: id)
+            .subscribe(onSuccess: { response in
+               self.profileList.accept(response)
+            }, onFailure: { error in
+                if let moyaError = error as? MoyaError {
+                    if let statusCode = moyaError.response?.statusCode {
+                        let networkError = NetworkError(rawValue: statusCode)
+                        switch networkError {
+                        case .invalidRequest:
+                            print("invalidRequest")
+                        default:
+                            print("network error")
+                        }
+                    }
+                }
+            })
             .disposed(by: disposeBag)
         
-        input.tapArticleButton
-            .emit()
-            .disposed(by: disposeBag)
-        
-        input.tapCommentsButton
-            .emit()
-            .disposed(by: disposeBag)
-        
-        return output
     }
 }
