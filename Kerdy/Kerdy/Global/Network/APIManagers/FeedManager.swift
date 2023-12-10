c953b0ec55632a8769d86272918d3cd2e6621b3d
jeongdung-eo <jeuniis2@gmail.com>
2023-12-11 00:55:15 +0900

    [Feat] 51 FeedManager 구현

---
+++ Kerdy/Kerdy/Global/Network/APIManagers/FeedManager.swift
@@ -0,0 +1,36 @@
+//
+//  FeedManager.swift
+//  Kerdy
+//
+//  Created by JEONGEUN KIM on 12/11/23.
+//
+
+import Foundation
+
+import RxSwift
+
+protocol FeedManagerType {
+
+    func getUserFeed() -> Single<[FeedResponseDTO]>
+}
+
+final class FeedManager: Networking, FeedManagerType {
+    
+    typealias API = FeedAPI
+    
+    static let shared = FeedManager()
+    
+    private let disposeBag = DisposeBag()
+    
+    private let provider = NetworkProvider<API>(plugins: [NetworkLogging()])
+    
+    private var authorizationCodeCallback: ((String) -> Void)?
+    
+    private init() {}
+    
+    func getUserFeed() -> Single<[FeedResponseDTO]> {
+        return provider
+            .request(.getUserFeed)
+            .map([FeedResponseDTO].self)
+    }
+}
