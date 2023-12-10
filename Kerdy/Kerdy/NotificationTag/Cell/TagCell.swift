9a222f2d586aa5f31ff7caf36e50fbdd5283c9ad
jeongdung-eo <jeuniis2@gmail.com>
2023-12-10 17:08:22 +0900

    [Feat] 45 TagCell 구현

--- Kerdy/Kerdy/NotificationTag/Cell/TagCell.swift
+++ Kerdy/Kerdy/NotificationTag/Cell/TagCell.swift
@@ -10,7 +10,25 @@ import UIKit
 import Core
 import SnapKit
 
+import RxSwift
+import RxCocoa
+
+enum TagType {
+    
+    case userTag, registerTag
+}
+
 final class TagCell: UICollectionViewCell {
+    
+    // MARK: - Proprerty
+    
+    private var type: TagType = .userTag {
+        didSet {
+            setBorderLayer()
+        }
+    }
+    
+    var disposeBag = DisposeBag()
     
     // MARK: - UI Property
     
@@ -21,22 +39,34 @@ final class TagCell: UICollectionViewCell {
         return label
     }()
     
+    fileprivate let cancelButton: UIButton = {
+        let button = UIButton()
+        button.setImage(.tagCancelIcon, for: .normal)
+        return button
+    }()
+    
     private let borderLayer = CAShapeLayer()
     
     // MARK: - Initialize
     
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        self.disposeBag = DisposeBag()
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: .zero)
         
-        setUI()
         setLayout()
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        setUI()
         
+        setBorderLayer()
     }
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -49,11 +79,11 @@ extension TagCell {
     
     private func setLayout() {
         
+        contentView.layer.addSublayer(borderLayer)
         contentView.addSubview(titleLabel)
         titleLabel.snp.makeConstraints {
             $0.center.equalToSuperview()
         }
-        
     }
     
     private func setUI() {
@@ -63,15 +93,66 @@ extension TagCell {
                                  topRight: 20,
                                  bottomLeft: 20,
                                  bottomRight: 12)
+    }
+    
+    private func setRegisterTag() {
         
         borderLayer.configureBorder(of: contentView,
                                     withStroke: .kerdyMain,
                                     withFill: .clear,
                                     using: 2)
+        
     }
     
-    func configureCell(to type: TagType) {
-        titleLabel.text = type.title
+    private func setUserTag() {
+        
+        titleLabel.sizeToFit()
+        
+        titleLabel.snp.remakeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().inset(20)
+        }
+        
+        contentView.addSubview(cancelButton)
+        cancelButton.snp.makeConstraints {
+            $0.leading.equalTo(titleLabel.snp.trailing).offset(10)
+            $0.centerY.equalToSuperview()
+            $0.trailing.lessThanOrEqualToSuperview().inset(15)
+            $0.size.equalTo(10)
+        }
+        
+        configureBackground()
     }
+    
+    private func setBorderLayer() {
+        
+        setUI()
+        type == .userTag ? setUserTag() : setRegisterTag()
+    }
+    
+    func configureCell(to type: TagsResponseDTO, tagType: TagType) {
+        
+        self.type = tagType
+        titleLabel.text = type.name
+        cancelButton.tag = type.id
+    }
+    
+    func configureBackground() {
+        
+        layoutIfNeeded()
+        borderLayer.configureBorder(of: contentView,
+                                    withStroke: .kerdyMain,
+                                    withFill: .kerdyMain,
+                                    using: 2)
+    }
+    
+}
 
+// MARK: - Reactive extension
+
+extension Reactive where Base: TagCell {
+    
+    var cancel: ControlEvent<Void> {
+        base.cancelButton.rx.tap
+    }
 }
