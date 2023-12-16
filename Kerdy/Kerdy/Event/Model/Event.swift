260de64d0563a4eddf8cf1c3b4dfd50cec0ef244
taipaise <taipaise@gmail.com>
2023-12-16 15:53:33 +0900

    feat 49 eventViewmodel행사 목록 관리기능 추가

--- Kerdy/Kerdy/Event/Model/Event.swift
+++ Kerdy/Kerdy/Event/Model/Event.swift
@@ -5,17 +5,27 @@
 //  Created by 이동현 on 10/29/23.
 //
 
-import UIKit
+import Foundation
 
-struct Event {
-    var name: String
-    var eventStatus: EventStatus // response를 보고 수정 예정
-    var img: UIImage
-    var tags: [String]
+struct Event: Codable {
+    let id: Int
+    let name: String
+    let informationUrl: String
+    let startDate: String
+    let endDate: String
+    let applyStartDate: String
+    let applyEndDate: String
+    let location: String
+    let tags: [String]
+    let thumbnailUrl: String?
+    let type: String
+    let imageUrls: [String]
+    let organization: String
+    let paymentType: String
+    let eventMode: String
 }
 
-enum EventStatus: Int {
-    case inProgress
-    case isFinished
-    case isPlanned
+func parseEvents(data: Data) throws -> [Event] {
+    let decoder = JSONDecoder()
+    return try decoder.decode([Event].self, from: data)
 }
--- Kerdy/Kerdy/Event/Viewmodel/EventViewModel.swift
+++ Kerdy/Kerdy/Event/Viewmodel/EventViewModel.swift
@@ -9,8 +9,52 @@ import Foundation
 
 import RxCocoa
 import RxSwift
-import Moya
 
 final class EventViewModel {
+    private let eventManager = EventManager.shared
+    private let disposeBag = DisposeBag()
+    private let filterRelay = BehaviorRelay<EventFilter>(value: EventFilter())
     
+    private var filterObservable: Observable<EventFilter> {
+        return filterRelay.asObservable().distinctUntilChanged()
+    }
+    
+    private let scrapEvents: BehaviorRelay<[Event]> = BehaviorRelay(value: [])
+    private let conferenceEvents: BehaviorRelay<[Event]> = BehaviorRelay(value: [])
+    private let competitionEvents: BehaviorRelay<[Event]> = BehaviorRelay(value: [])
+
+    var combinedEvents: Observable<[[Event]]> {
+        return Observable.zip(scrapEvents, conferenceEvents, competitionEvents) {
+            [$0, $1, $2]
+        }
+    }
+    
+    init() {
+        setupBindings()
+    }
+    
+    private func setupBindings() {
+        filterObservable
+            .flatMapLatest { [weak self] filter -> Observable<Void> in
+                guard let self = self else { return Observable.just(()) }
+                
+                return Observable.combineLatest(
+                    self.eventManager.getEvents(category: nil, eventFilter: filter),
+                    self.eventManager.getEvents(category: "CONFERENCE", eventFilter: filter),
+                    self.eventManager.getEvents(category: "COMPETITION", eventFilter: filter)
+                )
+                .map { scrapEvents, conferenceEvents, competitionEvents in
+                    self.scrapEvents.accept(scrapEvents)
+                    self.conferenceEvents.accept(conferenceEvents)
+                    self.competitionEvents.accept(competitionEvents)
+                }
+                .catchAndReturn(())
+            }
+            .subscribe()
+            .disposed(by: disposeBag)
+    }
+
+    func updateFilter(_ newFilter: EventFilter) {
+        filterRelay.accept(newFilter)
+    }
 }
