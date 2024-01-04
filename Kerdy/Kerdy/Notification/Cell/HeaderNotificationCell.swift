5ae2b9061fcac9f38f996be082e364504896c00b
jeongdung-eo <jeuniis2@gmail.com>
2024-01-05 03:07:20 +0900

    [Design] 67 header notification cell 구현

---
+++ Kerdy/Kerdy/Notification/Cell/HeaderNotificationCell.swift
@@ -0,0 +1,68 @@
+//
+//  CommonNotificationCell.swift
+//  Kerdy
+//
+//  Created by JEONGEUN KIM on 12/24/23.
+//
+
+import UIKit
+
+import RxSwift
+import RxCocoa
+
+final class HeaderNotificationCell: UICollectionViewListCell {
+    
+    // MARK: - Property
+    
+    var disposeBag = DisposeBag()
+    
+    // MARK: - UI Components
+    
+    fileprivate lazy var switchView: UISwitch = {
+        let view = UISwitch()
+        view.onTintColor = .kerdyMain
+        return view
+    }()
+    
+    // MARK: - init
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.disposeBag = DisposeBag()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Methods
+
+extension HeaderNotificationCell {
+    
+    private func setUI() {
+        
+        var content = defaultContentConfiguration()
+        content.text = Strings.receiveAllNotification
+        content.textProperties.font = .nanumSquare(to: .regular, size: 14)
+        contentConfiguration = content
+        accessories = [
+            .customView(configuration: .init(customView: switchView, placement: .trailing()))
+        ]
+    }
+}
+
+// MARK: - Reactive extension
+
+extension Reactive where Base: HeaderNotificationCell {
+    
+    var valueChanged: ControlProperty<Bool> {
+        base.switchView.rx.isOn
+    }
+}
