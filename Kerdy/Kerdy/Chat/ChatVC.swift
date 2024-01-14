2c01aacb55f0d4fa0fd436d6b1d9c5f436e8f38e
taipaise <taipaise@gmail.com>
2024-01-14 12:10:29 +0900

    [Design] 83 메시지목록vc UI 구현

--- Kerdy/Kerdy/Chat/ChatVC.swift
+++ Kerdy/Kerdy/Chat/ChatVC.swift
@@ -6,23 +6,77 @@
 //
 
 import UIKit
+import SnapKit
+import Core
 
-class ChatVC: UITabBarController {
-
+final class ChatVC: UITabBarController {
+    
+    private lazy var navigationView = UIView()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "메시지"
+        label.font = .nanumSquare(to: .bold, size: 14)
+        return label
+    }()
+    
+    private lazy var settingButton: UIButton = {
+        let button = UIButton()
+        button.setImage(.icMore, for: .normal)
+        button.setTitle(nil, for: .normal)
+        return button
+    }()
+    
+    private lazy var divideLine = DivideLine(frame: .zero, backgroundColor: .kerdyGray01)
+    
+    private lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUI()
+        setLayout()
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setUI() {
+        view.backgroundColor = .white
     }
-    */
+}
 
+// MARK: - layout 설정
+extension ChatVC {
+    private func setLayout() {
+        view.addSubviews(navigationView, collectionView)
+        navigationView.addSubviews(
+            titleLabel,
+            settingButton,
+            divideLine
+        )
+        
+        navigationView.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview()
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            $0.height.equalTo(56)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+        
+        settingButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(8)
+            $0.size.equalTo(24)
+        }
+        
+        divideLine.snp.makeConstraints {
+            $0.horizontalEdges.bottom.equalToSuperview()
+            $0.height.equalTo(1)
+        }
+        
+        collectionView.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview()
+            $0.top.equalTo(navigationView.snp.bottom)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+        }
+    }
 }
