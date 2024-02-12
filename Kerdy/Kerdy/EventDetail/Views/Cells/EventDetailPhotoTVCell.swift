6669f6e2109c7e631e41331d5d2faba0a17bf8eb
taipaise <taipaise@gmail.com>
2024-02-12 14:51:15 +0900

    [Feat] 63  cell configure 구현

--- Kerdy/Kerdy/EventDetail/Views/Cells/EventDetailPhotoTVCell.swift
+++ Kerdy/Kerdy/EventDetail/Views/Cells/EventDetailPhotoTVCell.swift
@@ -7,39 +7,64 @@
 
 import UIKit
 import SnapKit
+import RxSwift
 
 final class EventDetailPhotoTVCell: UITableViewCell {
     
-    private lazy var image = UIImageView()
- 
+    private lazy var infoImageView = UIImageView()
+    private var disposeBag = DisposeBag()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        setLayout()
         setUI()
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    func configure(with url: String) {
+        setImage(url: url)
+    }
 }
 
+// MARK: - layout 설정
 extension EventDetailPhotoTVCell {
-    private func setLayout() {
-        addSubview(image)
-        image.snp.makeConstraints {
+    private func setLayout(ratio: CGFloat) {
+        contentView.addSubview(infoImageView)
+        infoImageView.snp.makeConstraints {
             $0.edges.equalToSuperview()
+            $0.width.equalToSuperview()
+            $0.height.equalTo(contentView.snp.width).multipliedBy(ratio).priority(750)
         }
     }
     
     private func setUI() {
-        image.contentMode = .scaleAspectFit
+        infoImageView.contentMode = .scaleAspectFit
+        infoImageView.backgroundColor = .kerdyGray01
     }
 }
 
-extension EventDetailPhotoTVCell: ConfigurableCell {
-    typealias CellType = UIImage?
-    
-    func configure(with data: UIImage?) {
-        image.image = data
+// MARK: - 이미지 처리
+extension EventDetailPhotoTVCell {
+    func setImage(url: String) {
+        ImageManager.shared.getImage(url: url)
+            .subscribe { [weak self] image in
+                self?.infoImageView.image = image
+                let imageWidth = image.size.width
+                let imageHeight = image.size.height
+                let ratio = imageHeight / imageWidth
+                self?.setLayout(ratio: ratio)
+                
+                if let tableView = self?.superview as? UITableView {
+                    UIView.performWithoutAnimation {
+                        tableView.beginUpdates()
+                        tableView.endUpdates()
+                    }
+                }
+            } onFailure: { error in
+                print(error.localizedDescription)
+            }
+            .disposed(by: disposeBag)
     }
 }
---
+++ Kerdy/Kerdy/EventDetail/Views/Cells/EventDetailRecruitTVCell.swift
@@ -0,0 +1,77 @@
+//
+//  EventDetailRecruitTVCell.swift
+//  Kerdy
+//
+//  Created by 이동현 on 1/5/24.
+//
+
+import UIKit
+
+final class EventDetailRecruitTVCell: UITableViewCell {
+    
+    private lazy var memberNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .nanumSquare(to: .bold, size: 13)
+        return label
+    }()
+    
+    private lazy var contentLabel: UILabel = {
+        let label = UILabel()
+        label.font = .nanumSquare(to: .regular, size: 13)
+        label.textColor = .kerdyGray03
+        label.numberOfLines = 2
+        return label
+    }()
+    
+    private lazy var timeLabel: UILabel = {
+        let label = UILabel()
+        label.font = .nanumSquare(to: .regular, size: 11)
+        label.textColor = .kerdyGray02
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(with recruitment: RecruitmentResponseDTO) {
+        memberNameLabel.text = recruitment.member.name
+        contentLabel.text = recruitment.content
+        timeLabel.text = recruitment.updatedAt
+    }
+}
+
+// MARK: - layout 설정
+extension EventDetailRecruitTVCell {
+    private func setLayout() {
+        addSubviews(
+            memberNameLabel,
+            contentLabel,
+            timeLabel
+        )
+        
+        memberNameLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(15)
+            $0.leading.equalToSuperview().offset(17)
+        }
+        
+        contentLabel.snp.makeConstraints {
+            $0.top.equalTo(memberNameLabel.snp.bottom).offset(4)
+            $0.leading.equalTo(memberNameLabel.snp.leading)
+            $0.width.equalTo(210).priority(500)
+            $0.height.equalTo(30).priority(500)
+        }
+    
+        timeLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(17)
+            $0.leading.equalTo(memberNameLabel.snp.trailing).offset(5)
+            $0.width.equalTo(10).offset(500)
+            $0.height.equalTo(12)
+        }
+    }
+}
